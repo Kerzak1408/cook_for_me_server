649bf79ad3d23dedd4a692e57a1b37e702e4fa0d
Kerzak <lubomir.ohman@yahoo.com>
2017-05-27 22:16:15 +0200

    Improvements 27-05-17

--- Cook4Me/src/main/java/mprogramming/Cook4Me/Application.java
+++ Cook4Me/src/main/java/mprogramming/Cook4Me/Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication @ComponentScan({"Controller"})	
+import SocketServer.Server;
+
+@SpringBootApplication @ComponentScan({"Controller", "WebSockets"})	
 public class Application {
 
     public static void main(String[] args) {
+    	Server server = new Server();
+    	server.start();
         SpringApplication.run(Application.class, args);
     }
 }
